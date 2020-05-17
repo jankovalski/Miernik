c09e469b043ff2b0e108af60964d27e8cc83e632
jevgienij123 <46295168+jevgienij123@users.noreply.github.com>
2020-05-17 23:09:55 +0200

    added a second ADS1115 module

--- src/main.cpp
+++ src/main.cpp
@@ -4,34 +4,49 @@
 #include <Adafruit_ADS1015.h>
 
 Adafruit_ADS1115 ads(0x48);
+Adafruit_ADS1115 ads2(0x4A);
 LiquidCrystal_I2C lcd(PCF8574_ADDR_A21_A11_A01, 4, 5, 6, 16, 11, 12, 13, 14, POSITIVE);
+byte degree[8] = { 0b00110, 0b01001, 0b01001, 0b00110, 0b00000, 0b00000, 0b00000, 0b00000 };
 
-float vout1 = 0.0; float vout2 = 0.0; float vout3 = 0.0;
-float vin1 = 0.0; float vin2 = 0.0;
-float R1 = 110000.0; float R2 = 11000.0; float R3 = 110000.0; float R4 = 11000.0;
-float value1 = 0.0; float value2 = 0.0; float value3 = 0.0;
-float vmin1 = 20.0; float vmin2 = 20.0; float vmax1 = 0.0; float vmax2 = 0.0;
-float current; int currentmin; int currentmax;
-int avg = 10; int power; int powermin; int powermax;
+float R1 = 110000.0, R2 = 11000.0, R3 = 110000.0, R4 = 11000.0;
+float value1 = 0.0, value2 = 0.0, value3 = 0.0, value4 = 0.0, value5 = 0.0, vout1 = 0.0, vout2 = 0.0, vout3 = 0.0;
+float vin1 = 0.0, vin2 = 0.0, vmax1 = 0.0, vmax2 = 0.0, temp1 = 0.0, temp2 = 0.0;
+float average1 = 0.0, average2 = 0.0, average3 = 0.0, average4 = 0.0, average5 = 0.0;
+float vmin1 = 20.0, vmin2 = 20.0;
+int avg = 10, current = 0, currentmax = 0, power = 0, powermax = 0;
+int16_t adc0, adc1, adc2, adc20, adc21;
 
 void setup()
 {
   ads.begin();
+  ads2.begin();
   lcd.begin(16,4);
+  lcd.createChar(1, degree);
 }
 
 void loop()
 {
-  int16_t adc0; int16_t adc1; int16_t adc2;
   adc0 = ads.readADC_SingleEnded(0); adc1 = ads.readADC_SingleEnded(1); adc2 = ads.readADC_SingleEnded(2);
-  float average1 = 0; float average2 = 0; float average3 = 0;
+  adc20 = ads2.readADC_SingleEnded(1); adc21 = ads2.readADC_SingleEnded(2);
+  value1 = adc2; value2 = adc1; value3 = adc0;
+  temp1 = (adc20 * 0.1875)/10;
+  temp2 = (adc21 * 0.1875)/10;
+
+/*
+  average1 = 0.0; average2 = 0.0; average3 = 0.0; average4 = 0.0; average5 = 0.0;
+
   for (int i=0; i < avg; i++) {
     average1 = average1 + ads.readADC_SingleEnded(0);
     average2 = average2 + ads.readADC_SingleEnded(1);
     average3 = average3 + ads.readADC_SingleEnded(2);
+    average4 = average4 + ads2.readADC_SingleEnded(1);
+    average5 = average5 + ads2.readADC_SingleEnded(2);
   }
-  value1 = average1/avg; value2 = average2/avg; value3 = average3/avg;
-  value1 = adc1; value2 = adc2; value3 = adc0;
+
+  value1 = average2/avg; value2 = average3/avg; value3 = average1/avg; value4 = average4/avg; value5 = average5/avg;
+  temp1 = (value4 * 0.1875)/10; temp2 = (value5 * 0.1875)/10;
+*/
+
   vout1 = (value1 * 0.1875)/1000;
   vin1 = vout1 / (R2/(R1+R2));
   if (vin1 < 0.09) { vin1 = 0.0; }
@@ -42,13 +57,11 @@ void loop()
   if (vin2 < 0.09) { vin2 = 0.0; }
   if (vin2 < vmin2) { vmin2 = vin2; }
   if (vin2 > vmax2) { vmax2 = vin2; }
-  vout3 = (adc0 * 0.1875)/1000;
+  vout3 = (value3 * 0.1875)/1000;
   current = (vout3 - 2.541) / 0.02;
   if (current < 0.1) { current = 0; }
-  if (current < currentmin) { currentmin = current; }
   if (current > currentmax) { currentmax = current; }
   power = vin2 * current / 2;
-  if (power < powermin) { powermin = power; }
   if (power > powermax) { powermax = power; }
   lcd.setCursor(0, 0);  lcd.print(vmin1, 1);
   lcd.setCursor(0, 1);  lcd.print(vmin2, 1);
@@ -56,10 +69,10 @@ void loop()
   lcd.setCursor(6, 1);  lcd.print(vin2, 1);
   lcd.setCursor(12, 0); lcd.print(vmax1, 1);
   lcd.setCursor(12, 1); lcd.print(vmax2, 1);
-  lcd.setCursor(0,2);   lcd.print(currentmin); lcd.print("A  ");
+  lcd.setCursor(0,2);   lcd.print(round(temp1)); lcd.print(char(1)); lcd.print("C"); lcd.print(" ");
   lcd.setCursor(6,2);   lcd.print(current, 1); lcd.print("A  ");                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  
   lcd.setCursor(12,2);  lcd.print(currentmax); lcd.print("A  ");
-  lcd.setCursor(0,3);   lcd.print(powermin);   lcd.print("W  ");
+  lcd.setCursor(0,3);   lcd.print(round(temp2)); lcd.print(char(1)); lcd.print("C"); lcd.print(" ");
   lcd.setCursor(6,3);   lcd.print(power);      lcd.print("W  ");
   lcd.setCursor(12,3);  lcd.print(powermax);   lcd.print("W  ");
   delay(400);
